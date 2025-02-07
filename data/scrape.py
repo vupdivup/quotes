cadc758e50523525edbbc5b23aac63fa3125b5b3
vupdivup <vnmatt2003@gmail.com>
2025-02-07 10:44:34 +0100

    write get indices function

--- data/scrape.py
+++ data/scrape.py
@@ -1,21 +1,31 @@
 import requests
 import bs4
 
-url = "https://www.litquotes.com/quote_title_resp.php?TName=Allan%20Quatermain"
+url1 = "/quote_title_resp.php?TName=Allan%20Quatermain"
+url2 = "/quote_title_resp.php?TName=Dune"
 
-def get_quotes(url):
-    r = requests.get(url)
-    soup = bs4.BeautifulSoup(r.content, "html.parser")
+def soupify_page(url):
+    base = "https://www.litquotes.com"
+    r = requests.get(base + url)
+    return bs4.BeautifulSoup(r.content, "html.parser")
 
+def get_container(url):
+    soup = soupify_page(url)
+
+    # style selector, there isn't a more precise direct alternative
     selector = "div[style=\"" \
         "margin: 30px 20px 0px 20px; " \
         "font-size: 110%;\"]"
 
     # list of quotes
-    list = soup.css.select_one(selector)
+    return soup.css.select_one(selector)
+
+
+def get_quotes(url):
+    container = get_container(url)
 
     # all tags within quote list, including unneeded links etc.
-    tags = list(list.children)
+    tags = list(container.children)
 
     # group tags by quote
     groups = [
@@ -23,8 +33,17 @@ def get_quotes(url):
         for i in range(0, len(tags), 6)
     ]
 
+    # ensure compliance with quote group format
+    # this filters out leading and trailing misc tags
+    groups = [
+        g
+        for g in groups
+        if [t.name for t in g] == [None, "i", None, "a", None, "div"]
+    ]
+
     quotes = []
 
+    # extract quote data
     for g in groups:
         quotes.append({
             "quote": g[0].string,
@@ -34,4 +53,28 @@ def get_quotes(url):
 
     return quotes
 
-print(get_quotes(url))
+# print(get_quotes(url))
+
+def get_indices(url):
+    container = get_container(url)
+
+    tags = list(container.children)
+
+    # return if there's no navigation for more pages
+    if tags[-4].name != "div":
+        return [url]
+    
+    pagination = tags[-4]
+
+    links = pagination.find_all("a")
+
+    hrefs = [
+        l["href"]
+        for l in links
+        if l.string != "Next>>"
+    ]
+    
+    return [url, *hrefs]
+    
+
+# print(get_quotes(url2))
