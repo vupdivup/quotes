50fc093fa3d9b05eab9f048119ebf1f600e85c58
vupdivup <vnmatt2003@gmail.com>
2025-02-08 13:07:58 +0100

    add transform script

---
+++ data/transform.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+df = pd.read_json("data/quotes.json")
+
+df.quote = df.quote.apply(
+    # the split and rejoin removes any extra whitespace
+    lambda x: " ".join(x.split()).removesuffix(" ~")
+)
+
+# limit quote length
+df = df[
+    (df.quote.str.len() > 100) &
+    (df.quote.str.len() < 250)
+]
+
+dest = input("Destination: ")
+
+os.makedirs(os.path.dirname(dest), exist_ok=True)
+
+# write to json
+with open(dest + ".json", "w") as f:
+    df.to_json(f, orient="records", indent=4)
